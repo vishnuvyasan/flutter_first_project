47da133965601f01e71acf5fa78c28a9d7b856b1
Vishnu Nelliparambil Vyasan <vishnu@MOMAA.GOV.BH>
2020-03-03 12:04:11 +0300

    App are you married y/n

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,36 @@ import 'package:flutter/material.dart';
 void main() => runApp(FirstWidget());
 
 class FirstWidget extends StatelessWidget {
+  void isMarried() {
+    print("Congratulations");
+  }
+
+  void isNotMarried() {
+    print("go get married");
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ["Are you Married ?", "Do you have kids?"];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("Hello World!!!"),
         ),
-        body: Text("Body!!"),
+        body: Column(
+          children: <Widget>[
+            Text(questions[0]),
+            RaisedButton(
+              onPressed: isMarried,
+              child: Text("Yes"),
+            ),
+            RaisedButton(
+              onPressed: isNotMarried,
+              child: Text("No"),
+            ),
+          ],
+        ),
       ),
     );
   }
